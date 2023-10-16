411073df4b186b5ccba1fc50acddf5aa2785ee2b
Renttor <denis30510@gmail.com>
2023-10-16 15:39:34 +0300

    Created and set up project

---
+++ users/models.py
@@ -0,0 +1,14 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+NULLABLE = {'null': True, 'blank': True}
+
+
+class User(AbstractUser):
+    username = None
+
+    email = models.EmailField(unique=True, verbose_name='email')
+    phone = models.CharField(max_length=11, **NULLABLE, verbose_name='телефон')
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
